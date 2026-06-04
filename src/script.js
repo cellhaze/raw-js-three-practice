234251939340daf80bd9d8fb69ebf653da02cbf5
Cell <marcell.made@gmail.com>
2026-06-04 16:14:18 -0400

    tested materials; updated project folder

--- src/script.js
+++ src/script.js
@@ -1,151 +1,78 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import gsap from "gsap";
-import GUI from 'lil-gui'
-
-/**
- * Debug
- */
-
-// Can only update properties
-const gui = new GUI({
-  width: 400,
-  title: 'Updating Settings'
-})
-
-// To be able to changed variables, most create object
-const myDebugObject = {
-  myVariable: 1337
-}
-
-myDebugObject.color = '#c881e9'
-myDebugObject.subdivision = 2
-myDebugObject.spin = () => {
-  gsap.to(mesh.rotation, {duration: 1, y: mesh.rotation.y + Math.PI * 2})
-}
-gui.add(myDebugObject, 'spin')
-
-gui.add(myDebugObject, 'myVariable')
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
  */
-
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene();
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: myDebugObject.color, wireframe: true });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-gui.add(myDebugObject, 'subdivision')
-.min(1)
-.max(20)
-.step(1)
-.onFinishChange(() => {
-  console.log('subdivision changed')
-  mesh.geometry.dispose()
-  mesh.geometry = new THREE.BoxGeometry(
-    1, 1, 1,
-    myDebugObject.subdivision, myDebugObject.subdivision, myDebugObject.subdivision
-  )
-})
-
-gui.add(mesh.position, 'y')
-.min(-3)
-.max(3)
-.step(0.01)
-.name('Elevation')
-
-gui.add(mesh, 'visible')
-gui.add(material, 'wireframe')
-gui.addColor(myDebugObject, 'color')
-.name('Hex')
-.onChange((value) => {
-  //console.log(material.color)
-  //console.log(value)
-  //console.log(value.getHexString())
-  material.color.set(myDebugObject.color)
-})
+const scene = new THREE.Scene()
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-// Resize
-window.addEventListener("resize", () => {
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-// Allow double-click to go fullscreen
-window.addEventListener("dblclick", () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-});
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100,
-);
-camera.position.z = 3;
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
 
 // Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
  */
-const clock = new THREE.Clock();
+const clock = new THREE.Clock()
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-  // Update controls
-  controls.update();
+    // Update controls
+    controls.update()
 
-  // Render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera)
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
 
-tick();
+tick()
